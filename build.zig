5413d1afd430cc175de297fd09f26c2a9beef075
Andrea Feletto <andrea@andreafeletto.com>
2024-08-24 23:27:37 +0200

    Update to zig 0.13

--- build.zig
+++ build.zig
@@ -10,11 +10,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zon2nix",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.addOptions("options", options);
+    exe.root_module.addOptions("options", options);
     exe.linkLibC();
     b.installArtifact(exe);
 
@@ -27,11 +27,11 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    unit_tests.addOptions("options", options);
+    unit_tests.root_module.addOptions("options", options);
     unit_tests.linkLibC();
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
