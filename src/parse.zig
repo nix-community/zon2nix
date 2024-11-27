686882cfd3b241667db36fd47c952cb153145677
Matei Dibu <contact@mateidibu.dev>
2024-11-27 12:44:23 +0200

    add test for dep with deps with no deps

--- src/parse.zig
+++ src/parse.zig
@@ -103,11 +103,14 @@ test parse {
     try parse(alloc, &deps, basic);
     basic.close();
 
-    try testing.expectEqual(deps.count(), 4);
+    try testing.expectEqual(deps.count(), 5);
     try testing.expectEqualStrings(deps.get("122048992ca58a78318b6eba4f65c692564be5af3b30fbef50cd4abeda981b2e7fa5").?.url, "https://github.com/ziglibs/known-folders/archive/fa75e1bc672952efa0cf06160bbd942b47f6d59b.tar.gz");
     try testing.expectEqualStrings(deps.get("122089a8247a693cad53beb161bde6c30f71376cd4298798d45b32740c3581405864").?.url, "https://github.com/ziglibs/diffz/archive/90353d401c59e2ca5ed0abe5444c29ad3d7489aa.tar.gz");
     try testing.expectEqualStrings(deps.get("1220363c7e27b2d3f39de6ff6e90f9537a0634199860fea237a55ddb1e1717f5d6a5").?.url, "https://gist.github.com/antlilja/8372900fcc09e38d7b0b6bbaddad3904/archive/6c3321e0969ff2463f8335da5601986cf2108690.tar.gz");
-    const git_dep = deps.get("1220115ff095a3c970cc90fce115294ba67d6fbc4927472dc856abc51e2a1a9364d7").?;
-    try testing.expectEqualStrings(git_dep.url, "https://github.com/kristoff-it/ziggy");
-    try testing.expectEqualStrings(git_dep.rev, "c66f47bc632c66668d61fa06eda112b41d6e5130");
+    const ziggy = deps.get("1220115ff095a3c970cc90fce115294ba67d6fbc4927472dc856abc51e2a1a9364d7").?;
+    try testing.expectEqualStrings(ziggy.url, "https://github.com/kristoff-it/ziggy");
+    try testing.expectEqualStrings(ziggy.rev, "c66f47bc632c66668d61fa06eda112b41d6e5130");
+    const vaxis = deps.get("1220feaa655e14cbb4baf59fe746f09a17fc6949be46ad64dd5044982f4fc1bb57c7").?;
+    try testing.expectEqualStrings(vaxis.url, "https://github.com/rockorager/libvaxis");
+    try testing.expectEqualStrings(vaxis.rev, "1fd920a7aea1bb040c7c028f4bbf0af2ea58e1d1");
 }
