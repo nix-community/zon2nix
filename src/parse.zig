f5864c4c72ea59daece9d9b9e26b6f1ef11cd1dd
Matei Dibu <contact@mateidibu.dev>
2025-03-04 12:49:32 +0200

    add test for git+https with "?ref=..." dependency

--- src/parse.zig
+++ src/parse.zig
@@ -103,7 +103,7 @@ test parse {
     defer basic.close();
     try parse(alloc, &deps, basic);
 
-    try testing.expectEqual(deps.count(), 5);
+    try testing.expectEqual(deps.count(), 6);
     try testing.expectEqualStrings(deps.get("122048992ca58a78318b6eba4f65c692564be5af3b30fbef50cd4abeda981b2e7fa5").?.url, "https://github.com/ziglibs/known-folders/archive/fa75e1bc672952efa0cf06160bbd942b47f6d59b.tar.gz");
     try testing.expectEqualStrings(deps.get("122089a8247a693cad53beb161bde6c30f71376cd4298798d45b32740c3581405864").?.url, "https://github.com/ziglibs/diffz/archive/90353d401c59e2ca5ed0abe5444c29ad3d7489aa.tar.gz");
     try testing.expectEqualStrings(deps.get("1220363c7e27b2d3f39de6ff6e90f9537a0634199860fea237a55ddb1e1717f5d6a5").?.url, "https://gist.github.com/antlilja/8372900fcc09e38d7b0b6bbaddad3904/archive/6c3321e0969ff2463f8335da5601986cf2108690.tar.gz");
@@ -113,4 +113,7 @@ test parse {
     const vaxis = deps.get("1220feaa655e14cbb4baf59fe746f09a17fc6949be46ad64dd5044982f4fc1bb57c7").?;
     try testing.expectEqualStrings(vaxis.url, "https://github.com/rockorager/libvaxis");
     try testing.expectEqualStrings(vaxis.rev, "1fd920a7aea1bb040c7c028f4bbf0af2ea58e1d1");
+    const zig_tracy = deps.get("122094fc39764bd527269d3721f52fc3b8cbb72bc4cdbd3345cbc2cd941936f3d185").?;
+    try testing.expectEqualStrings(zig_tracy.url, "https://github.com/vancluever/zig-tracy?ref=fix-callstack");
+    try testing.expectEqualStrings(zig_tracy.rev, "6e123ee26032e49a1a0039524ddf7970692931d9");
 }
