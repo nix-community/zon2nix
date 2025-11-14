ceff2fe7ebf0f6c97025ec83dd3fdc99ededc0b7
David Nies <david.nies@posteo.de>
2025-11-15 00:29:04 +0100

    Add `format` method to `Dependency`.
    Prior to this change, there was no such method.
    
    This change adds it as it comes in handy during debugging (you can print it with `{f}` now).

--- src/Dependency.zig
+++ src/Dependency.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Writer = std.Io.Writer;
 const Dependency = @This();
 
 url: []const u8,
@@ -10,4 +11,8 @@ pub fn deinit(self: Dependency, alloc: std.mem.Allocator) void {
     alloc.free(self.url);
     if (self.rev) |rev| alloc.free(rev);
     if (self.nix_hash) |nix_hash| alloc.free(nix_hash);
+}
+
+pub fn format(self: Dependency, writer: *Writer) Writer.Error!void {
+    try writer.print("Dependency{{ .url = {s}, .rev = {?s}, .nix_hash = {?s}, .done = {} }}", .{self.url, self.rev, self.nix_hash, self.done});
 }
